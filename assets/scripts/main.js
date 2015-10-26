1f1c25e66341c0f3c1f8d991e7fe879556ead7ab
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-10-26 10:43:58 +0100

    Add vendor prefixes to menu animation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,22 +1,47 @@
 (function () {
   'use strict'
-
+  
+  /**
+   * Sets the transformProp to whatever the current webbrowser should work with.
+   * 
+   * Credit to user hitesh kumar on StackOverflow
+   * http://stackoverflow.com/a/31539834/3593017
+   */
+  var transformProp = (function () {
+    var testEl = document.createElement('div');
+    if (testEl.style.transform == null) {
+      var vendors = ['Webkit', 'Moz', 'ms'];
+      for (var i = 0; i < vendors.length; i++) {
+        var vendor = vendors[i];
+        if (testEl.style[vendor + 'Transform'] !== undefined) {
+          testEl = null;
+          return vendor + 'Transform';
+        }
+      }
+    }
+    testEl = null;
+    return 'transform';
+  })();
+  
   var hamburger = document.getElementById('hamburger');
   var pushmenu = document.getElementById('pushmenu');
 
   function hamburgerClickHandler(e) {
     if (/ open|open /g.test(pushmenu.className)) {
       pushmenu.className = pushmenu.className.replace(/open/, '');
-      document.body.style.transform = 'translateX(0)';
+      document.body.style[transformProp] = 'translateX(0)';
+      // if (transformProp === 'msTransform')
     } else {
       pushmenu.className += ' open';
-      console.log(document.body.style.transform);
-      document.body.style.transform = 'translateX(-250px)';
+      document.body.style[transformProp] = 'translateX(-250px)';
+      // if (transformProp === 'msTransform')
     }
   }
-
-  // Add event listeners to hamburger menu.
-  if ('addEventListener' in hamburger) { hamburger.addEventListener('click', hamburgerClickHandler); }
-  if ('attachEvent' in hamburger) { hamburger.attachEvent('onclick', hamburgerClickHandler, false); }
-
+  
+  // Both may exist, and this attaches only one of them.
+  if ('addEventListener' in hamburger) {
+    hamburger.addEventListener('click', hamburgerClickHandler);
+  } else if ('attachEvent' in hamburger) {
+    hamburger.attachEvent('onclick', hamburgerClickHandler, false);
+  }
 })();
